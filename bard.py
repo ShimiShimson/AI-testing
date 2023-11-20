a9d171e82150e8056ce81cf09471af9704824091
Shimi Shimson <szymon.jakub.adamczuk@gmail.com>
2023-11-20 03:04:41 +0100

    still trying...

--- bard.py
+++ bard.py
@@ -21,21 +21,25 @@ for i in range(num_cells):
 # Define the size of each cell
 cell_size = 50
 
+
 # Add content to the world
-def generate_landmass(x_pos, y_pos, shape):
+def generate_landmass(x, y, shape):
     # Generate a random shape for the landmass
     shape = ("oval", "square")
     # Set the size and position of the landmass
-    size = (100, 50)
-    position = (x_pos, y_pos)
+    position = (x * cell_size, y * cell_size)
+    x_pos, y_pos = position
 
     # Draw the landmass according to the chosen shape
     if shape == "oval":
-        pygame.draw.ellipse(screen, (50, 150, 50), position, size)
+        pygame.draw.ellipse(screen, (50, 150, 50), (x_pos, y_pos, 100, 50))
+        pygame.display.flip()
     else:
-        x_pos, y_pos = position
-        width, height = size
-        pygame.draw.rect(screen, (50, 150, 50), (x_pos, y_pos), (width, height))
+        # Extract the x and y coordinates from the tuple
+
+        # Pass the coordinates as separate arguments
+        pygame.draw.rect(screen, (50, 150, 50), (x_pos, y_pos, 100, 50))
+        pygame.display.flip()
 
     return shape, position
 
@@ -51,9 +55,23 @@ def generate_mountains(x, y):
     # Draw the mountains according to the chosen shape
     if shape == "lines":
         for i in range(5):
-            pygame.draw.line(screen, (0, 0, 0), (position[0] + random.randint(0, size[0]), position[1] + i * 5), (position[0] + size[0] - random.randint(0, size[0]), position[1] + i * 5), 3)
+            pygame.draw.line(
+                screen,
+                (0, 0, 0),
+                (position[0] + random.randint(0, size[0]), position[1] + i * 5),
+                (
+                    position[0] + size[0] - random.randint(0, size[0]),
+                    position[1] + i * 5,
+                ),
+                3,
+            )
+            pygame.display.flip()
+
     else:
-        pygame.draw.rect(screen, (0, 0, 0), position, size)
+        pygame.draw.rect(
+            screen, (0, 0, 0), (position[0], position[1], size[0], size[1])
+        )
+        pygame.display.flip()
 
     return shape, position
 
@@ -68,9 +86,16 @@ def generate_oceans(x_pos, y, shape):
     # Draw the oceans according to the chosen shape
     if shape == "rect":
         pygame.draw.rect(screen, (0, 0, 150), position, size)
+        pygame.display.flip()
+
     else:
-        points = [(position[0], position[1] + size[1]), (position[0] + size[0], position[1] + size[1] / 2), (position[0] + size[0] / 2, position[1])]
+        points = [
+            (position[0], position[1] + size[1]),
+            (position[0] + size[0], position[1] + size[1] / 2),
+            (position[0] + size[0] / 2, position[1]),
+        ]
         pygame.draw.polygon(screen, (0, 0, 150), points)
+        pygame.display.flip()
 
     return shape, position
 
@@ -79,16 +104,19 @@ def generate_oceans(x_pos, y, shape):
 for i in range(num_cells):
     # Generate a landmass
     x, y = grid[i]  # Unpack the grid cell tuple
-    shape, position = generate_landmass(x, y)
+    # Add the shape argument to the generate_landmass() call
+    shape, position = generate_landmass(x, y, "oval")
     # Add the landmass to the grid
     grid.append([position[0] + cell_size, position[1] + cell_size])
 
     # Generate fewer mountains and oceans around the landmass for better performance
     for j in range(2):
-        shape, position = generate_mountains(grid[i][0] + random.randint(-5, 5), grid[i][1] + random.randint(-5, 5))
+        shape, position = generate_mountains(
+            grid[i][0] + random.randint(-5, 5), grid[i][1] + random.randint(-5, 5)
+        )
         grid.append([position[0], position[1]])
 
-        shape, position = generate_oceans(grid[i][0], grid[i][1])
+        shape, position = generate_oceans(grid[i][0], grid[i][1], "triangle")
         grid.append([position[0], position[1]])
 
 # Create a game loop
@@ -97,5 +125,4 @@ while True:
     if pygame.key.get_pressed()[pygame.K_ESCAPE]:
         pygame.quit()
         sys.exit()
-
-    
+    pygame.display.flip()
--- chatgpt.py
+++ chatgpt.py
@@ -12,8 +12,8 @@ screen = pygame.display.set_mode(world_size)
 # Create a list to store the grid cells
 grid = []
 
-# Generate a random number of cells in the grid
-num_cells = 100
+# Generate a reduced number of cells for better performance
+num_cells = 50
 for i in range(num_cells):
     cell = [random.randint(0, world_size[0]), random.randint(0, world_size[1])]
     grid.append(cell)
@@ -21,61 +21,104 @@ for i in range(num_cells):
 # Define the size of each cell
 cell_size = 50
 
+
 # Add content to the world
-def generate_landmass(x, y):
+def generate_landmass(x, y, shape):
+    # Generate a random shape for the landmass
+    shape = ("oval", "square")
     # Set the size and position of the landmass
-    size = (100, 50)
     position = (x * cell_size, y * cell_size)
-    return "landmass", position
+    x_pos, y_pos = position
+
+    # Draw the landmass according to the chosen shape
+    if shape == "oval":
+        pygame.draw.ellipse(screen, (50, 150, 50), (x_pos, y_pos, 100, 50))
+    else:
+        pygame.draw.rect(screen, (50, 150, 50), (x_pos, y_pos, 100, 50))
+
+    return shape, position
+
 
 # Add mountains and oceans
 def generate_mountains(x, y):
+    # Generate a random shape for the mountain range
+    shape = ("lines", "square")
     # Set the size and position of the mountain range
-    size = (100, 50)
+    size = (50, 25)
     position = (x * cell_size, y * cell_size)
-    return "mountains", position
 
-def generate_oceans(x, y):
+    # Draw the mountains according to the chosen shape
+    if shape == "lines":
+        for i in range(5):
+            pygame.draw.line(
+                screen,
+                (0, 0, 0),
+                (position[0] + random.randint(0, size[0]), position[1] + i * 5),
+                (
+                    position[0] + size[0] - random.randint(0, size[0]),
+                    position[1] + i * 5,
+                ),
+                3,
+            )
+    else:
+        pygame.draw.rect(
+            screen, (0, 0, 0), (position[0], position[1], size[0], size[1])
+        )
+
+    return shape, position
+
+
+def generate_oceans(x_pos, y, shape):
+    # Generate a random shape for the ocean
+    shape = ("rect", "triangle")
     # Set the size and position of the ocean
     size = (100, 50)
-    position = (x * cell_size, y * cell_size)
-    return "oceans", position
+    position = (x_pos, y)
+
+    # Draw the oceans according to the chosen shape
+    if shape == "rect":
+        pygame.draw.rect(screen, (0, 0, 150), position, size)
+    else:
+        points = [
+            (position[0], position[1] + size[1]),
+            (position[0] + size[0], position[1] + size[1] / 2),
+            (position[0] + size[0] / 2, position[1]),
+        ]
+        pygame.draw.polygon(screen, (0, 0, 150), points)
+
+    return shape, position
+
 
 # Add content to the world
 for i in range(num_cells):
     # Generate a landmass
-    grid.append(generate_landmass(grid[i][0], grid[i][1]))
+    x, y = grid[i]  # Unpack the grid cell tuple
+    # Add the shape argument to the generate_landmass() call
+    shape, position = generate_landmass(x, y, "oval")
 
-    # Generate mountains and oceans around the landmass
-    for j in range(5):
-        grid.append(generate_mountains(grid[i][0] + random.randint(-5, 5), grid[i][1] + random.randint(-5,5)))
-        grid.append(generate_oceans(grid[i][0] + random.randint(-5, 5), grid[i][1] + random.randint(-5, 5)))
+    # Generate fewer mountains and oceans around the landmass for better performance
+    for j in range(2):
+        shape, position = generate_mountains(
+            grid[i][0] + random.randint(-5, 5), grid[i][1] + random.randint(-5, 5)
+        )
+        grid.append([position[0], position[1]])
+
+        shape, position = generate_oceans(grid[i][0], grid[i][1], "triangle")
+        grid.append([position[0], position[1]])
+
+    pygame.display.flip()  # Move this outside the inner loop
 
 # Create a game loop
-clock = pygame.time.Clock()
-while True:
-    # Check for key presses
+running = True
+while running:
+    # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
 
-    # Clear the screen and draw the world
-    screen.fill((255, 255, 255))
-    for entry in grid:
-        if isinstance(entry, int):  # Treat integers as a placeholder for landmass
-            obj_type, (position_x, position_y) = "landmass", (entry, 0)
-        else:
-            obj_type, (position_x, position_y) = entry
+    # Check for key presses
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        running = False
 
-        # Draw different shapes and colors based on object type
-        if obj_type == "landmass":
-            pygame.draw.rect(screen, (0, 255, 0), (position_x, position_y, cell_size, cell_size))
-        elif obj_type == "mountains":
-            pygame.draw.lines(screen, (255, 0, 0), False, [(position_x, position_y), (position_x + cell_size, position_y)], 2)
-        elif obj_type == "oceans":
-            pygame.draw.polygon(screen, (0, 0, 255), [(position_x, position_y), (position_x + cell_size, position_y), (position_x + cell_size/2, position_y + cell_size)], 0)
-
-    # Update the game state
     pygame.display.flip()
-    clock.tick(60)
